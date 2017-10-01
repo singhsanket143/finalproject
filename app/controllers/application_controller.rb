6262cf4be7980b5449523b8d6c24a9825b9131c5
sanjeet <you@example.com>
2017-10-01 16:02:00 +0530

    related ques added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,23 @@ def addnotes
   @question=Question.new
 
 end
+def sideques
+    @questagfeed = []
+if params[:question_id]
+           @question=Question.find(params[:id])
+      
+        @question.tag_list.each do |tag| 
+          @questagfeed += Question.tagged_with(tag) 
+        end
+        else
+          @tftags=Question.tag_counts_on(:tags).order('count desc').limit(5)
+          @tftags.each do |tag| 
+           @questagfeed += Question.tagged_with(tag) 
+          end
+
+        end
+
+  end
 
   protected
   def configure_permitted_parameters
