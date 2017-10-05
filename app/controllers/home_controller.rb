dd855cb594423a0ff6d17ead1f1a96dbe8616fce
Sanket <singhsanket143@gmail.com>
2017-10-05 18:44:33 +0530

    login signup dedicATED

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,42 +14,41 @@ class HomeController < ApplicationController
           @feed=Question.search(params[:search]).paginate(:per_page => 20, :page => params[:page])
         else
           @feed=current_user.feed.paginate(:per_page => 7, :page => params[:page])
-          end
-          }
-          format.js {}
         end
-        end
-
-        def indexmain
-          respond_to do |format|
-            format.html {
-              @question = Question.new
-
-              if params[:tag]
-                @feed = Question.tagged_with(params[:tag]).paginate(:per_page => 4, :page => params[:page])
-              else
-                @feed=current_user.feed
-                @latestfeed=current_user.latestfeed
-               
-
-
-                @tftags=Question.tag_counts_on(:tags).order('count desc').limit(5)
-                @trendingfeed=[]
-
-                @tftags.each do |tag|
-                  @trendingfeed += Question.tagged_with(tag)
-                end
-              end
-            }
-            format.js {}
-          end
-        end
-
-        def users_list
-          @users=User.all
-        end
-
-        def tags_list
-
-        end
+      }
+      format.js {}
+    end
   end
+
+  def indexmain
+    respond_to do |format|
+      format.html {
+        @question = Question.new
+
+        if params[:tag]
+          @feed = Question.tagged_with(params[:tag]).paginate(:per_page => 4, :page => params[:page])
+        else
+          @feed=current_user.feed
+          @latestfeed=current_user.latestfeed
+
+
+          @tftags=Question.tag_counts_on(:tags).order('count desc').limit(5)
+          @trendingfeed=[]
+
+          @tftags.each do |tag|
+            @trendingfeed += Question.tagged_with(tag)
+          end
+        end
+      }
+      format.js {}
+    end
+  end
+
+  def users_list
+    @users=User.all
+  end
+
+  def tags_list
+
+  end
+end
